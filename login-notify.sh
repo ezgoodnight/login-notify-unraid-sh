3fd7dfbd8458a4ae69782e4b20301cf9d7e822bb
Eric Z Goodnight <ezgoodnight@gmail.com>
2025-10-15 14:37:57 -0400

    rewrite for single if statement

--- login-notify.sh
+++ login-notify.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 tail -fn0 /var/log/syslog | \
 while read line ; do
-        echo "$line" | grep "Unsuccessful login"
+        echo "$line" | grep "uccessful login"
         if [ $? = 0 ]
         then
-           myuser=`echo "$line" | awk -F " " '{print $9}'`
-           /usr/local/emhttp/webGui/scripts/notify -s "Unraid failed login attempt" -i "alert" -m "${myuser} just failed to log into Unraid"  -d "${myuser} just failed to log into Unraid"
+           myuser=(`echo $line | awk -F " " '{print $6, $9}'`)
+           /usr/local/emhttp/webGui/scripts/notify -s "${myuser[0]} Unraid login attempt" -i "alert" -m "${myuser[0]} login attempt to UNRAID by ${myuser[1]}"  -d "${myuser[0]} login by user ${myuser[1]}"
         fi
-        echo "$line" | grep "Successful login"
-        if [ $? = 0 ]
-        then
-           myuser=`echo "$line" | awk -F " " '{print $9}'`
-           /usr/local/emhttp/webGui/scripts/notify -s "Unraid successful login" -i "alert" -m "${myuser} just logged into Unraid"  -d "${myuser} just logged into Unraid"
-        fi
+        
 done
