929cab38bf8e56707d49df8cf2f72b478c3f7d89
Eric Z Goodnight <ezgoodnight@gmail.com>
2025-10-17 12:16:23 -0400

    readability

--- login-notify.sh
+++ login-notify.sh
@@ -4,9 +4,9 @@ while read line ; do
         echo "$line" | grep "uccessful login" #currently syslog uses this format for logins, works for Successful and Unsuccessful logins
         if [ $? = 0 ]
         then
-           myuser=(`echo $line | awk -F " " '{print $6, $9}'`) #index 6 and 9 of the awk command should be Successful/Unsuccessful and the username attempted
-           #puts the login attempt success state and user used into the array myuser
-           /usr/local/emhttp/webGui/scripts/notify -s "${myuser[0]} Unraid login attempt" -i "alert" -m "${myuser[0]} login attempt to UNRAID by ${myuser[1]}"  -d "${myuser[0]} login by user ${myuser[1]}"
+           notify_array=(`echo $line | awk -F " " '{print $6, $9}'`) #index 6 and 9 of the awk command should be Successful/Unsuccessful and the username attempted
+           #puts the login attempt success state and user used into the array notify_array
+           /usr/local/emhttp/webGui/scripts/notify -s "${notify_array[0]} Login ${notify_array[1]}" -i "alert" -m "${notify_array[0]} login attempt to UNRAID by ${notify_array[1]}"  -d "${notify_array[0]} login by user ${notify_array[1]}"
         fi
         
 done
