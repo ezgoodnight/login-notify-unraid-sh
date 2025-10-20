e778a65fd8d9316c7340fdbfbce5320f0c2b9731
Eric Z Goodnight <ezgoodnight@gmail.com>
2025-10-20 11:29:55 -0400

    added IP address to body of message

--- login-notify.sh
+++ login-notify.sh
@@ -4,9 +4,10 @@ while read line ; do
         echo "$line" | grep "uccessful login" #currently syslog uses this format for logins, works for Successful and Unsuccessful logins
         if [ $? = 0 ]
         then
-           notify_array=(`echo $line | awk -F " " '{print $6, $9}'`) #index 6 and 9 of the awk command should be Successful/Unsuccessful and the username attempted
+           notify_array=(`echo $line | awk -F " " '{print $6, $9, $11}'`) #index 6 and 9 of the awk command should be Successful/Unsuccessful and the username attempted
            #puts the login attempt success state and user used into the array notify_array
-           /usr/local/emhttp/webGui/scripts/notify -s "${notify_array[0]} Login ${notify_array[1]}" -i "alert" -m "${notify_array[0]} login attempt to UNRAID by ${notify_array[1]}"  -d "${notify_array[0]} login by user ${notify_array[1]}"
+           #added the IP address from syslog into body of message
+           /usr/local/emhttp/webGui/scripts/notify -s "${notify_array[0]} Login ${notify_array[1]}" -i "alert" -m "${notify_array[0]} login attempt to UNRAID by ${notify_array[1]}"  -d "${notify_array[0]} login by user ${notify_array[1]} at IP ${notify_array[2]}"
         fi
         
 done
